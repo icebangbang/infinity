e1cd293912c7bf88fd5ea167ae68a1f72f899f9f
icebangbang <lifeng.ye@ecreditpal.com>
2022-04-09 19:56:47 +0800

    本地部署脚本12

--- run-local.sh
+++ run-local.sh
@@ -12,19 +12,19 @@ echo '##################################'
 docker build --no-cache -f ./CerelyDockerfile -t dao-celery:latest .
 
 docker stop dao-celery-1
-docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-1 -e "index=${1}" -e "route=default" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
+docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-1 -e "index=1" -e "route=default" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
 
 docker stop dao-celery-2
-docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-2 -e "index=${2}" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
+docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-2 -e "index=2" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
 
 docker stop dao-celery-3
-docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-3 -e "index=${3}" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
+docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-3 -e "index=3" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
 
 docker stop dao-celery-4
-docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-4 -e "index=${4}" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
+docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-4 -e "index=4" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
 
 docker stop dao-celery-5
-docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-5 -e "index=${4}" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
+docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-5 -e "index=5" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
 
 
 for ((index3=6; index3<=11; index3++))
