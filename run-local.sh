7d177928ad0fec16c7efc90b8d918f1c71f508a9
icebangbang <lifeng.ye@ecreditpal.com>
2022-04-09 13:26:17 +0800

    本地部署脚本8

--- run-local.sh
+++ run-local.sh
@@ -1,20 +1,19 @@
 docker build  -f ./CerelyScheduleDockerfile -t dao-celery-schedule:latest .
 
 docker stop  dao-celery-schedule-1
-docker rmi dao-celery-schedule-1:latest
 docker run -d --privileged=true  -e "index=1"  -e "FLASK_ENV=test" --name=dao-celery-schedule-1  --net=host --rm -it dao-celery-schedule:latest
 
 docker build -f ./Dockerfile -t dao:latest .
+echo '##################################'
 docker stop dao
-docker rmi dao:latest
 docker run -d --privileged=true -v /var/log/dao:/var/log/dao -e "profiles=test" --name=dao -e "index=1" -e "port=20500" --net=host --rm -it dao:latest
+echo '##################################'
 
 docker build --no-cache -f ./CerelyDockerfile -t dao-celery:latest .
 
 for ((index=1; index<=1; index++))
 do
 docker stop dao-celery-${index}
-docker rmi dao-celery-${index}:latest
 docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-${index} -e "index=${index}" -e "route=default" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
 echo ${index}
 done
@@ -22,7 +21,6 @@ done
 for ((index=2; index<=5; index++))
 do
 docker stop dao-celery-${index}
-docker rmi dao-celery-${index}:latest
 docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-${index} -e "index=${index}" -e "route=day_level" -e "FLASK_ENV=test" -e "thread=50" --net=host --rm -it dao-celery:latest
 echo ${index}
 done
@@ -30,7 +28,6 @@ done
 for ((index=6; index<=11; index++))
 do
 docker stop dao-celery-${index}
-docker rmi dao-celery-${index}:latest
 docker run -d --privileged=true  -e "profiles=test" --name=dao-celery-${index} -e "index=${index}" -e "route=indicator" -e "FLASK_ENV=test" -e "thread=20" --net=host --rm -it dao-celery:latest
 echo ${index}
 done
