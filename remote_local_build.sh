2efafde47252691b57ae19833239549276b75cb7
icebangbang <lifeng.ye@ecreditpal.com>
2022-04-29 16:04:54 +0800

    远程话本地部署调试

--- remote_local_build.sh
+++ remote_local_build.sh
@@ -3,7 +3,7 @@
 # sh run-cerely.sh ~/Work/pem/xzhh.pem development test 6 15 indicator true
 # sh run-cerely.sh ~/Work/pem/xzhh.pem development test 1 1 default true
 
-# sh remote_local_build.sh ~/Work/pem/ironmansre_cdh.pem offline offline zz_lf Gitee158,
+# sh remote_local_build.sh ~/Work/pem/ironmansre_cdh.pem offline offline /home/ironmansre zz_lf Gitee158,
 
 
 
@@ -11,16 +11,18 @@
 pem=${1}
 env=${2}
 target=${3}
-gituser=${4}
-gitpass=${5}
+dest=${4}
+gituser=${5}
+gitpass=${6}
 
 
 
-ansible-playbook \
+/usr/local/bin/ansible-playbook \
   deploy/remote_site.yml \
   -i deploy/hosts \
   --private-key ${pem} \
   -e env=${env} \
   -e target=${target} \
+  -e dest=${dest} \
   -e gituser=${gituser} \
   -e gitpass=${gitpass}
