44438e3b85f656d4a56765d805c90658376af9ab
icebangbang <lifeng.ye@ecreditpal.com>
2022-06-15 18:25:56 +0800

    特征跑批调整

--- app/main/rest/indicator.py
+++ app/main/rest/indicator.py
@@ -1,12 +1,16 @@
+from app.main.stock.dao import stock_dao
 from app.main.utils import date_util
 from . import rest
 from app.main.utils import restful
 from app.main.utils import simple_util
+import collections
+
 from flask import request
 from app.main.db.mongo import db
 from datetime import datetime
 from app.main.stock.chart import chart_instance_dict
 import pandas as pd
+
 
 @rest.route("/indicator/ppi", methods=['get'])
 def get_ppi():
@@ -45,6 +49,7 @@ def get_ppi():
     return restful.response(data=dict(date=date, current=current, grace=grace,
                                       accum=accum, cpi=cpi))
 
+
 @rest.route("/indicator/pmi", methods=['get'])
 def get_pmi():
     """
@@ -55,11 +60,9 @@ def get_pmi():
     ppi = db['pmi']
     ppi_list = list(ppi.find({"date": {"$gte": start}}).sort("_id", -1))
 
-
     date = []
     zzy = []
     fzzy = []
-
 
     for i, p in enumerate(ppi_list):
         date.append(date_util.date_time_to_str(p['date'], fmt='%Y-%m'))
@@ -67,6 +70,7 @@ def get_pmi():
         fzzy.append(float(p['fzzy_value']))
 
     return restful.response(data=dict(date=date, zzy=zzy, fzzy=fzzy))
+
 
 @rest.route("/indicator/sow", methods=['get'])
 def get_sow_data():
@@ -88,6 +92,7 @@ def get_sow_data():
         date.append(date_util.date_time_to_str(sow['date'], fmt='%Y-%m'))
     return restful.response(data=dict(date=date, num=num, tb=tb, hb=hb))
 
+
 @rest.route("/indicator/chart", methods=['get'])
 def chart_display():
     """
@@ -100,6 +105,7 @@ def chart_display():
 
     return restful.response(data=data)
 
+
 @rest.route("/indicator/marketStatus/current", methods=['get'])
 def current_market_status():
     """
@@ -110,8 +116,8 @@ def current_market_status():
     result = latest_point['distribution']
     result['rateMedian'] = latest_point['rate_median']
 
-
     return restful.response(data=result)
+
 
 @rest.route("/indicator/upLimit", methods=['get'])
 def upLimit_detail():
@@ -122,18 +128,40 @@ def upLimit_detail():
     date = date_util.get_latest_work_day()
     stock_feature = db['stock_feature']
     results = list(stock_feature.find({"features.cont_up_limit_count": {"$gte": 1},
-                                 "date": date}))
+                                       "date": date}))
+    codes = [result['code'] for result in results]
+
+    stock_map = stock_dao.get_stock_detail(codes)
 
     stocks = [dict(name=result['name'],
+                   code=result['code'],
                    cont_up_limit_count=result['features']['cont_up_limit_count']) for result in results]
     df = pd.DataFrame(stocks)
     df['cut'] = pd.cut(df.cont_up_limit_count, bins=[0, 1, 2, 3, 4, 5, 100], labels=["1", "2", "3", "4", "5", ">=6"],
                        include_lowest=False)
 
-    # group_result = {cut: group.to_dict('records') for cut, group in df.groupby('cut')}
-    group_result = [dict(time=cut, stocks= [ r['name'] for r in group.to_dict('records')]) for cut, group in df.groupby('cut')]
+    group_result = []
+    for cut, group in df.groupby('cut'):
+        records = group.to_dict('records')
+
+        industries = []
+        for record in records:
+            stock = stock_map[record['code']]
+
+            if "industry" in stock.keys():
+                industries.append(stock['industry'])
+        counter = collections.Counter(industries)
+
+        item = dict(time=cut, stocks=[r['name'] for r in records],
+                    industries=counter.most_common(len(counter)))
+
+        group_result.append(item)
+
+    # group_result = [dict(time=cut, stocks= [ r['name'] for r in group.to_dict('records')])
+    #                 for cut, group in df.groupby('cut')]
 
     return restful.response(data=group_result)
+
 
 @rest.route("/indicator/upLimitBack", methods=['get'])
 def upLimit_back_detail():
@@ -144,17 +172,19 @@ def upLimit_back_detail():
     date = date_util.get_latest_work_day()
     stock_feature = db['stock_feature']
     results = list(stock_feature.find({"features.continuous_up_limit_stop": True,
-                                       "features.continuous_up_limit_count_before":{"$gte":1},
-                                 "date": date}))
+                                       "features.continuous_up_limit_count_before": {"$gte": 1},
+                                       "date": date}))
 
     stocks = [dict(name=result['name'],
-                   continuous_up_limit_count_before=result['features']['continuous_up_limit_count_before']) for result in results]
+                   continuous_up_limit_count_before=result['features']['continuous_up_limit_count_before']) for result
+              in results]
     df = pd.DataFrame(stocks)
-    df['cut'] = pd.cut(df.continuous_up_limit_count_before, bins=[0, 1, 2, 3, 4, 5, 100], labels=["1", "2", "3", "4", "5", ">=6"],
+    df['cut'] = pd.cut(df.continuous_up_limit_count_before, bins=[0, 1, 2, 3, 4, 5, 100],
+                       labels=["1", "2", "3", "4", "5", ">=6"],
                        include_lowest=False)
 
     # group_result = {cut: group.to_dict('records') for cut, group in df.groupby('cut')}
-    group_result = [dict(time=cut, stocks= [ r['name'] for r in group.to_dict('records')]) for cut, group in df.groupby('cut')]
+    group_result = [dict(time=cut, stocks=[r['name'] for r in group.to_dict('records')]) for cut, group in
+                    df.groupby('cut')]
 
     return restful.response(data=group_result)
-
