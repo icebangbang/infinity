968f392374c40a55ed10aa2af229d8468f3e6741
icebangbang <lifeng.ye@ecreditpal.com>
2022-07-13 22:09:15 +0800

    add job chain

--- app/main/stock/dao/task_dao.py
+++ app/main/stock/dao/task_dao.py
@@ -4,29 +4,31 @@ from app.main.db.mongo import db
 from datetime import datetime
 from app.main.utils import my_redis
 import json
+import logging as log
 
 
 def create_task(task_id, task_path, size, chain=None):
     now = datetime.now()
     sync_record = db['sync_record']
 
-    data = dict(create_time=now, size=size, task_path=task_path,chain=chain)
+    data = dict(create_time=now, size=size, task_path=task_path, chain=chain)
 
-    sync_record.update_one({"task_id": task_id,"task_path":task_path}, {"$set": data}, upsert=True)
+    sync_record.update_one({"task_id": task_id, "task_path": task_path}, {"$set": data}, upsert=True)
     my_redis.set(task_id, size)
 
     if chain:
         my_redis.set("chain_" + task_id, json.dumps(chain))
 
 
-def update_task(task_id, size, task_path=None,next_kwargs=None):
+def update_task(task_id, size, task_path=None, next_kwargs=None):
     i = my_redis.incrby(task_id, -size)
     if i <= 0:
         now = datetime.now()
         sync_record = db['sync_record']
-        sync_record.update_one({"task_id": task_id,"task_path":task_path}, {"$set": {"update_time": now}})
+        sync_record.update_one({"task_id": task_id, "task_path": task_path}, {"$set": {"update_time": now}})
 
         chain_json = my_redis.get("chain_" + task_id)
+        log.info(chain_json)
         if chain_json:
             chain: list = json.loads(chain_json)
 
@@ -36,13 +38,12 @@ def update_task(task_id, size, task_path=None,next_kwargs=None):
             # 执行下一个任务
             next = chain[index + 1]
             p, m = next.rsplit('.', 1)
-
-            m = getattr(importlib.import_module(p),
-                                m, None)
+            log.info("module {} m {}".format(p, m))
+            method = getattr(importlib.import_module(p),
+                        m, None)
 
             next_kwargs = next_kwargs if next_kwargs is not None else dict(global_task_id=task_id)
-            m.apply_async(kwargs=next_kwargs)
-
+            method.apply_async(kwargs=next_kwargs)
 
 
 if __name__ == "__main__":
