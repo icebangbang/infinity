0176c8660d023382601bb98a92266d29a3f38fea
icebangbang <lifeng.ye@ecreditpal.com>
2022-08-20 14:47:19 +0800

    自定义板块筛选

--- app/main/stock/chart/SingleTrendAnaysis.py
+++ app/main/stock/chart/SingleTrendAnaysis.py
@@ -1,3 +1,5 @@
+import numpy
+
 from app.main.stock.chart.Line import Line
 from app.main.db.mongo import db
 from app.main.utils import date_util
@@ -7,38 +9,37 @@ import pandas as pd
 from app.main.utils.date_util import WorkDayIterator
 
 
-class TrendAnaysis(Line):
+class SingleTrendAnaysis(Line):
     """
     趋势分析表
     """
 
     def generate(self, **kwargs):
-        industryStart = int(kwargs['industryStart'])
-        industryEnd = int(kwargs['industryEnd'])
-        trend = kwargs['trend']
+        industry = kwargs['industry']
 
-        board_detail = db['board_detail']
-        boards = board_detail.find({"type": 2}).sort("size",-1).skip(industryStart).limit(industryEnd - industryStart)
-        industrys  = [board['board'] for board in boards]
-        legend = dict(data= industrys)
-
+        legend = dict(
+            data=['up','down','enlarge','convergence'],
+            selected=dict(up=True,down=True,enlarge=False,convergence=False)
+        )
         trend_data = db['trend_data']
         end = date_util.get_latest_work_day()
-        start = date_util.get_work_day(end, 30)
+        start = date_util.get_work_day(end, 90)
 
         data_x = [date_util.date_time_to_str(date, "%m-%d") for date in WorkDayIterator(start, end)]
 
-        trend_data_list = list(trend_data.find({"industry": {"$in": industrys},
+        trend_data_list = list(trend_data.find({"industry": industry,
                                                 "date": {"$gte": start, "$lte": end},
-                                                "trend": trend
                                                 }))
-        data_y_array = [dict(name="", y=[], yAxisIndex=0) for i in range(len(industrys))]
+        data_y_array = [dict(name="", y=[], yAxisIndex=0) for i in range(4)]
         index = 0
         df = pd.DataFrame(trend_data_list)
-        for key, group in df.sort_values("date",ascending=True).groupby('industry'):
+        for key, group in df.sort_values("date", ascending=True).groupby('trend'):
             data_y_array[index]['name'] = key
             for point in group.to_dict("records"):
                 data_y_array[index]['y'].append(point['rate'])
+            if key == 'up' or key == 'down':
+                median = numpy.median(data_y_array[index]['y'])
+                data_y_array[index]['markLine'] = {"data": [{"yAxis": median, "name": "中位数"}]}
             index = index + 1
         # y轴组合
         yAxis_array = [
@@ -48,11 +49,12 @@ class TrendAnaysis(Line):
             }
         ]
 
-        return dict(x=data_x, y_array=data_y_array, desc="板块趋势", multiSerie=True,
-                    yAxis_array=yAxis_array, legend=legend)
+        return dict(x=data_x, y_array=data_y_array, desc=industry, multiSerie=True,
+                    yAxis_array=yAxis_array, legend=legend,
+                    )
 
 
 if __name__ == "__main__":
-    trend = TrendAnaysis()
+    trend = SingleTrendAnaysis()
     trend.generate(trend="up",
-                   industryStart=0,industryEnd=10)
+                   industryStart=0, industryEnd=10)
