14a6dc0d68cf9fd9e88531279c2bbc7444f989f5
icebangbang <lifeng.ye@ecreditpal.com>
2022-09-12 22:15:05 +0800

    财务数据同步bug fix

--- app/main/rest/celery.py
+++ app/main/rest/celery.py
@@ -21,6 +21,7 @@ def performance():
     stock_task.sync_profit.apply_async(args=[])
     stock_task.sync_cash_flow.apply_async(args=[])
     stock_task.sync_balance.apply_async(args=[])
+    stock_task.sync_analysis_indicator.apply_async(args=[])
 
     return restful.response("ok")
 
