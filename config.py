b6b76dbba6e7bc1201b1504b31e13c2e1cc1c570
icebangbang <lifeng.ye@ecreditpal.com>
2023-01-02 21:58:37 +0800

    nacos 配置

--- config.py
+++ config.py
@@ -129,6 +129,18 @@ class Infinity(Config):
     RESULT_BACKEND = 'redis://:ironBackRedis123@0.0.0.0:30004/1'
     MONGO_URL = "mongodb://root:whosyourdaddy$879@0.0.0.0:20017/"
 
+    SERVER_HOST = "10.8.0.2"
+    SERVER_PORT = 20060
+    NACOS_SERVICE_NAME = "infinity"
+    NACOS_CLUSTER_NAME = "DEFAULT"
+
+    REGISTERED_SERVER_TO_NACOS = True
+    NACOS_SERVER_ADDRESSES = "10.8.0.2:20048"
+    NACOS_NAMESPACE = "public"
+    IS_AUTH_MODE = False
+    NACOS_WEIGHT = 1
+    NACOS_HEARTBEAT_INTERVAL = 6
+
 
 class InfinityLocal(Config):
     """
