80e46cd7a6e70901619a491fafd0e8ca8d0f0ced
zz_lf <787036156@qq.com>
2023-05-10 23:00:26 +0800

    设置任务回调打印日志

--- app/main/stock/dao/task_dao.py
+++ app/main/stock/dao/task_dao.py
@@ -46,12 +46,11 @@ def update_task(task_id, size, task_name=None, job_params=None):
         sync_record = db['sync_record']
 
         job_json_info = job_config.load_job_config(task_id)
-
         job_info = None
         if job_json_info is not None:
             job_info = json.loads(job_json_info)
-            log.info(job_json_info)
-        log.info("chain_" + task_id)
+        else:
+            log.info("[任务更新]:任务配置信息为空")
         if job_info and job_info['job_type'] == task_constant.TASK_TYPE_CELERY:
             job_chain: list = job_info['job_chain']
             for job_path in job_chain:
--- app/main/stock/job/job_config.py
+++ app/main/stock/job/job_config.py
@@ -1,5 +1,8 @@
 from app.main.utils import my_redis, date_util
 import json
+import logging
+
+log = logging.getLogger(__name__)
 
 
 def check_status_available(task_path):
@@ -32,9 +35,13 @@ def release_job(task_path):
 
 
 def set_job_config(task_id, chain_job_config):
-    my_redis.set("job_config" + str(task_id), json.dumps(chain_job_config, ensure_ascii=False))
+    config_json = json.dumps(chain_job_config, ensure_ascii=False)
+    log.info("向缓存中写入配置:{},task_id:{}",config_json,task_id)
+    my_redis.set("job_config" + str(task_id), config_json)
     my_redis.expire("job_config" + str(task_id), 60 * 60)
 
 
 def load_job_config(task_id,**kwargs):
-    return my_redis.get("job_config" + str(task_id))
+    config_json = my_redis.get("job_config" + str(task_id))
+    log.info("从缓存中读取配置内容:{},task_id:{}", config_json, task_id)
+    return config_json
