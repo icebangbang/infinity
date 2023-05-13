ec24d91836168f1b9de83fc93ce208621f20d649
zz_lf <787036156@qq.com>
2023-05-13 20:49:45 +0800

    dao模块部署调整

--- run-dao-local.sh
+++ run-dao-local.sh
@@ -9,7 +9,7 @@ env=${1}
 
 docker stop dao
 docker build --network=host --build-arg PIP_MIRROR=${PIP_MIRROR} --build-arg PIP_MIRROR_HOST=${PIP_MIRROR_HOST} -f ./Dockerfile -t dao:latest .
-docker run -d --privileged=true -v /var/log/dao:/var/log/dao -e "profiles=${env}" --name=dao -e "index=1" -e "port=20500" -v /etc/localtime:/etc/localtime:ro --net=host --rm -it dao:latest
+docker run -d --privileged=true -v /var/log/dao:/var/log/dao -e "profiles=${env}" --name=dao -e "index=1" -e "port=20500" -v /etc/localtime:/etc/localtime:ro -p 20500:20500 --rm -it dao:latest
 #docker build --network=host --build-arg PIP_MIRROR=http://127.0.0.1:7104/root/pypi/+simple/ --build-arg PIP_MIRROR_HOST=127.0.0.1:7104 -f ./Dockerfile -t dao:latest .
 #docker run -d --privileged=true -v /var/log/dao:/var/log/dao -e "profiles=infinity" --name=dao -e "index=1" -e "port=20500" -v /etc/localtime:/etc/localtime:ro --net=host --rm -it dao:latest
 
