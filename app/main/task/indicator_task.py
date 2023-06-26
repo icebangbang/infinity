df32479e00949770a5fbf4b2530227ee2385bce1
lifeng <787036156@qq.com>
2023-06-26 10:31:13 +0800

    feat: 同步中国货币供应量

--- app/main/task/indicator_task.py
+++ app/main/task/indicator_task.py
@@ -1,13 +1,45 @@
 """
 汇率相关任务
 """
-import logging as log
+import logging
 from datetime import datetime
 
 import akshare as ak
 import pandas as pd
 
 from app.main.db.mongo import db
+
+log = logging.getLogger(__name__)
+
+
+def china_supply_of_money():
+    """
+    国家统计局-货币供应量
+    :return:
+    """
+    log.info("开始同步中国货币供应量")
+    df = ak.macro_china_supply_of_money()
+    df = pd.DataFrame(df[['统计时间', '货币和准货币（广义货币M2）', '货币(狭义货币M1)',
+                          '流通中现金(M0)', '活期存款',
+                          '准货币', '定期存款', '储蓄存款', '其他存款']])
+
+    df.columns = ['date',
+                  'm2',
+                  'm1',
+                  'm0',
+                  'demand_deposit',
+                  'near_money',
+                  'time_deposit',
+                  'savings_deposit',
+                  'other_deposit'
+                  ]
+
+    df['date'] = pd.to_datetime(df['date'], format='%Y.%m')
+    df['update'] = datetime.now()
+    db['china_supply_of_money'].drop()
+    db['china_supply_of_money'].insert_many(df.to_dict(orient="records"))
+
+
 
 
 def sync_cny_fx():
@@ -67,14 +99,14 @@ def sync_fed_interest_rate():
     db['fed_interest_rate'].insert_many(result_dict_list)
 
     for result_dict in result_dict_list:
-
         db['calendar_event'].update_one({"title": "美联储利率决议",
                                          "start": result_dict['date']},
-                                        {"$set": dict(source="美联储",start=result_dict['date'],title="美联储利率决议")},
+                                        {"$set": dict(source="美联储", start=result_dict['date'], title="美联储利率决议")},
                                         upsert=True)
 
 
 if __name__ == "__main__":
     sync_comex_gold()
+    china_supply_of_money()
     # sync_cny_fx()
     pass
